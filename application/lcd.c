a8bdbad3d33dc51d24f9312b770a2ec6483b06c4
RombieHHH <rombielhuang@gmail.com>
2026-09-07 16:02:47 +0800

    init_2

--- application/lcd.c
+++ application/lcd.c
@@ -3,5 +3,13 @@
 #include "main.h"
 #include "cmsis_os.h"
 
-void LCDTaskFunc(void *argument){
+void LCDTaskFunc(void *argument)
+{
+    (void)argument;
+
+    for (;;)
+    {
+        /* TODO: Add LCD updates here. */
+        osDelay(1);
+    }
 }
--- application/led.c
+++ application/led.c
@@ -3,5 +3,13 @@
 #include "main.h"
 #include "cmsis_os.h"
 
-void LEDTaskFunc(void *argument){
+void LEDTaskFunc(void *argument)
+{
+    (void)argument;
+
+    for (;;)
+    {
+        /* TODO: Add LED control here. */
+        osDelay(1);
+    }
 }
